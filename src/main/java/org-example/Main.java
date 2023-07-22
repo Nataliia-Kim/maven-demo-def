704d1dd1655bb293fd84b7cb55798c57244a453f
Nataliia-Kim <kimnataliiait@gmail.com>
2023-07-22 15:15:34 +0300

    Initial Commit

---
+++ src/main/java/org-example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+import java.util.logging.*;
+public class Main {
+    public static void main(String[] args) {
+      Logger logger = Logger.getLogger(Main.class.getName());
+      logger.info("Hello World!");
+    }
+}
